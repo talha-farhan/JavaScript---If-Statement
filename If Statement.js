a40de943088c93447e010468d3bac74edefca6ea
Talha Farhan <128322158+talha-farhan@users.noreply.github.com>
2023-07-18 00:21:19 +0500

    Create If Statement.js

---
+++ If Statement.js
@@ -0,0 +1,52 @@
+var a = 10;
+var b = 20;
+var c = 20;
+var d = "10";
+var age = 16;
+let rating = 3;
+let random = MATH.random();
+
+if(a > b){
+    document.write("Mission Passed");
+}
+//It is not necessary to write semi-colon here
+document.write("<br>")
+
+if(a < c){
+    document.write("Mission Aborted");
+}
+document.write("<br>")
+
+if(b = c){
+    document.write("Some Success");
+}
+document.write("<br>")
+
+if(a == d){
+    document.write("D Day")
+}
+document.write("<br>")
+
+if(a === d){
+    document.write("Dragon Ball Zee")
+}
+document.write("<br>")
+
+if(age < 18){
+    document.write("Too young to buy cigarettes")
+}
+document.write("<br>")
+
+if(rating === 3){
+    document.write("You are a superstar!");
+}
+document.write("<br>")
+
+if (1+1 == 2){
+    document.write("Maths still works!!");
+}
+document.write("<br>")
+
+if (random < 0.5){
+    document.write("Your number is less than 0.5!!");
+}
